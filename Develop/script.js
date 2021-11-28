ee9743576d58bcd32a306f2e2e9ae8b198308637
Sam Rapowitz <samrapow@gmail.com>
2021-11-28 00:42:05 -0500

    Dynamically changes color depending on time

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,28 @@ var currentDay = $('#currentDay');
 var today = moment();
 var timeblocks = $('.container');
 var rows = $('.row');
+var nowHour = parseInt(moment().format('H'));
 
+// Print current day in requested format
 currentDay.text(today.format("dddd, MMMM Do"));
 
+// Loop through rows to set color
+rows.each(function(i) {
+    var rowID = this.id;
+    var rowHour;
+
+    if(rowID) {
+        rowHour = parseInt(rowID);
+    }
+
+    var thisRow = $(this);
+    if (rowHour) {
+        if (nowHour === rowHour) {
+            thisRow.addClass('present');
+        } else if (nowHour > rowHour) {
+            thisRow.addClass('past');
+        } else if (nowHour < rowHour) {
+            thisRow.addClass('future');
+        }
+    }
+})
